1e8becbb9772cd2b90020fd39d4db768859fa0c1
fe2s <aka.fe2s@gmail.com>
2016-06-13 04:37:23 -0400

    better run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [ ! -f ./target/github-helper.jar ]; then
+if [ ! -f $DIR/target/github-helper.jar ]; then
     echo "Jar not found ... building"
     mvn clean package
 fi
 
-java -jar ./target/github-helper.jar
+java -jar $DIR/target/github-helper.jar
